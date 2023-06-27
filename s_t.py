84cc29827a9c49a8aca08be1b3b2ceb7c18ea704
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-06-27 11:23:31 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -1,36 +1,20 @@
 import streamlit as st
 from bokeh.models.widgets import Button
 from bokeh.models import CustomJS
-from streamlit_bokeh_events import streamlit_bokeh_events
 
-stt_button = Button(label="Speak", width=100)
+text = st.text_input("Say what ?")
 
-stt_button.js_on_event("button_click", CustomJS(code="""
-    var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
- 
-    recognition.onresult = function (e) {
-        var value = "";
-        for (var i = e.resultIndex; i < e.results.length; ++i) {
-            if (e.results[i].isFinal) {
-                value += e.results[i][0].transcript;
-            }
-        }
-        if ( value != "") {
-            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-        }
-    }
-    recognition.start();
+tts_button = Button(label="Speak", width=100)
+
+tts_button.js_on_event("button_click", CustomJS(code=f"""
+    var u = new SpeechSynthesisUtterance();
+    u.text = "{text}";
+    u.lang = 'en-US';
+
+    speechSynthesis.speak(u);
     """))
 
-result = streamlit_bokeh_events(
-    stt_button,
-    events="GET_TEXT",
-    key="listen",
-    refresh_on_update=False,
-    override_height=75,
-    debounce_time=0)
+st.bokeh_chart(tts_button)
 
 if result:
     if "GET_TEXT" in result:
