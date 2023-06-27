c57d8fbe2f8c093af67f69586c1ef3b3363b06e9
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-06-27 11:33:02 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -2,9 +2,9 @@ import streamlit as st
 from bokeh.models.widgets import Button
 from bokeh.models import CustomJS
 
-text = st.text_input("Say what ?")
+text = st.text_input("Que he dicho ?")
 
-tts_button = Button(label="Speak", width=100)
+tts_button = Button(label="Habla", width=100)
 
 tts_button.js_on_event("button_click", CustomJS(code=f"""
     var u = new SpeechSynthesisUtterance();
