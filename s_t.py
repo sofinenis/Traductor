6e72559408d88082e700c9c1dce53ce771ae7a49
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-07-19 13:47:32 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -1,7 +1,12 @@
+import os
 import streamlit as st
 from bokeh.models.widgets import Button
 from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
+
+from langchain.agents import create_pandas_dataframe_agent
+from langchain.llms import OpenAI
+from apikey import apikey
 
 st.title("Interfaces Multimodales Audio y Texto")
 
@@ -53,7 +58,18 @@ result = streamlit_bokeh_events(
 if result:
     if "GET_TEXT" in result:
         st.write(result.get("GET_TEXT"))
-        
+        if st.button('Traducir'):
+           os.environ["OPENAI_API_KEY"] = "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"
+           prompt = “Traduce al inglés : " + result.get("GET_TEXT")
+           response = openai.Completion.create(
+           engine=“text-davinci-002”,
+           prompt=prompt,
+           temperature=0.2,
+           max_tokens=50,
+           n=1,
+           stop=None,
+           context=conversation_id,
+           ) 
 
         
     
