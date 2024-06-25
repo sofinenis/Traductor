cfbe557e782db0b61aa6fab250886471b47d72c3
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-06-25 12:42:45 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -31,7 +31,7 @@ with st.sidebar:
 
 st.write("Toca el Botón y habla lo que quires traducir")
 
-stt_button = Button(label=" Escuchar 	🎙️ ", width=300,  height=50)
+stt_button = Button(label=" Escuchar ", width=300,  height=50)
 
 stt_button.js_on_event("button_click", CustomJS(code="""
     var recognition = new webkitSpeechRecognition();
